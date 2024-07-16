ead8541ea84f7f2dcbbedd2ab3d59ec1f2888416
이예은 <gkfn1121123@gmail.com>
2024-07-16 20:51:37 +0900

    feat: apply new font style

--- src/components/Keyword/Keyword.jsx
+++ src/components/Keyword/Keyword.jsx
@@ -6,7 +6,7 @@ const Keyword = ({ keyword, isSelected, onSelectKeyword }) => (
     className={`flex w-24 h-[35px] items-center justify-center gap-2.5 px-4 py-px relative ${isSelected ? `bg-blue-300` : `bg-[#fffafa]`} rounded-[10px] cursor-pointer hover:bg-[#B9FFFF]`}
     onClick={onSelectKeyword}
   >
-    <div className="relative w-fit ml-[-8.00px] mr-[-8.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-black text-base tracking-[0] leading-[normal] whitespace-nowrap">
+    <div className="relative w-fit ml-[-8.00px] mr-[-8.00px] text-black text-base tracking-[0] leading-[normal] whitespace-nowrap">
       # {keyword}
     </div>
   </div>
