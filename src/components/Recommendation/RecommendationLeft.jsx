4ff19ad2abdc86f32f4f78bc5fb2f7002128bdde
이예은 <gkfn1121123@gmail.com>
2024-07-16 18:28:04 +0900

    ResultPage 반응형 디자인 추가

--- src/components/Recommendation/RecommendationLeft.jsx
+++ src/components/Recommendation/RecommendationLeft.jsx
@@ -1,10 +1,12 @@
 // src/components/Recommendation/RecommendationLeft.jsx
-import React from 'react';
+import React from "react";
 
 const RecommendationLeft = ({ result }) => (
-  <div className="text-2xl font-bold pl-20">
-    우리의 만남 장소는<br />
-    <span className="text-4xl">{result['station_name']}</span><br />
+  <div className="absolute top-0 right-3 lg:left-3 lg:top-20 min-w-fit text-2xl font-bold pl-20">
+    우리의 만남 장소는
+    <br />
+    <span className="text-4xl">{result["station_name"]}</span>
+    <br />
     어때요?
   </div>
 );
--- src/components/Recommendation/RecommendationRight.jsx
+++ src/components/Recommendation/RecommendationRight.jsx
@@ -1,43 +1,38 @@
 // src/components/Recommendation/RecommendationRight.jsx
-import React from 'react';
-import table from '../../assets/images/table.png';
-import kakao from '../../assets/images/kakao.png';
-import naver from '../../assets/images/naver.png';
+import React from "react";
+import table from "../../assets/images/table.png";
+import kakao from "../../assets/images/kakao.png";
+import naver from "../../assets/images/naver.png";
 
 const RecommendationRight = ({ result, comment, onShare }) => (
-  <div>
-    <div className="text-3xl font-bold mb-5">GPT 요약</div>
-    <div className="mb-5 text-xl">
-      {comment}
+  <div className="flex flex-col h-full">
+    <div className="absolute top-12">
+      <div className="text-3xl font-bold mb-5">GPT 요약</div>
+      <div className="mb-5 text-xl flex-grow pr-2">
+        {comment}
+      </div>
     </div>
-    <div className="flex gap-2">
+    <div className="absolute bottom-3 flex gap-2 mt-auto">
       <a href="https://naver.com">
-        <img
-          src={table}
-          alt="Table"
-          className="w-8 h-8"
-        />
+        <img src={table} alt="Table" className="w-8 h-8" />
       </a>
       <a href="https://naver.com">
-        <img
-          src={kakao}
-          alt="Kakao"
-          className="w-8 h-8"
-        />
+        <img src={kakao} alt="Kakao" className="w-8 h-8" />
       </a>
       <a href="https://naver.com">
-        <img
-          src={naver}
-          alt="Naver"
-          className="w-8 h-8"
-        />
+        <img src={naver} alt="Naver" className="w-8 h-8" />
       </a>
-      <button className="bg-blue-500 text-white px-2 py-1 rounded">
-        <a href={`https://map.kakao.com/link/to/${result['station_name']},${result['coordinates']['y']},${result['coordinates']['x']}`}>
+      <button className="bg-blue-500 text-white h-10 min-w-fit px-2 py-1 rounded">
+        <a
+          href={`https://map.kakao.com/link/to/${result["station_name"]},${result["coordinates"]["y"]},${result["coordinates"]["x"]}`}
+        >
           길찾기
         </a>
       </button>
-      <button className="bg-green-500 text-white px-2 py-1 rounded" onClick={onShare}>
+      <button
+        className="bg-green-500 text-white h-10 min-w-fit px-2 py-1 rounded"
+        onClick={onShare}
+      >
         공유하기
       </button>
     </div>
