fb456d3be59f67538a046c97b6d6ccb404601911
minseo25 <ms059333@gmail.com>
2024-07-25 22:38:39 +0900

    https issue

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const backendInstance = axios.create({
-  baseURL: "http://ec2-52-64-207-15.ap-southeast-2.compute.amazonaws.com:8080",
+  baseURL: "https://ec2-52-64-207-15.ap-southeast-2.compute.amazonaws.com:8080",
   headers: {
     "Content-Type": "application/json"
   }
